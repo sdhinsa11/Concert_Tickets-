5623db6549bf61319416df3e7850122dda331849
SDHINSA <sohanad@icloud.com>
2024-02-26 19:32:58 -0700

    Finding events through the api

--- main.py
+++ main.py
@@ -7,9 +7,26 @@ load_dotenv() # load env vars from.env file
 apikey = os.getenv("apikey") # gets the api key
 
 
-url = "https://app.ticketmaster.com/discovery/v2/events.json"
+url = "https://app.ticketmaster.com/discovery/v2/events.json?"
 
 params = {
-    "size":1,
-    "apikey": "{apikey}"
+    "apikey": apikey,
+    "countryCode": "CA",
+    "city": "Vancouver"
+    # "city": "Edmonton"
+    
 }
+
+try: 
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    json_data = response.json() # parse json
+    
+    events = json_data["_embedded"]["events"]
+
+    for event in events:
+        print("Name:", event["name"])
+    
+
+except requests.exceptions.RequestException as e:
+    print("Error fetching data:", e)
